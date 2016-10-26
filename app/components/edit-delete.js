884b68c5139f88b38f8b322566dd2e36c43b0689
Ryan Loos and Stephen Newkirk <stephen@>
2016-10-26 14:09:54 -0700

    add delete function

---
+++ app/components/edit-delete.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    deletePost(post) {
+      if(confirm('Are you sure you want to delete this post? (Have you checked with the Elder Gods yet?)')) {
+        this.sendAction('deletePost', post);
+      }
+    }
+  }
+});
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('post');
+  },
   actions: {
     savePost(params) {
       var newPost = this.store.createRecord('post', params);
       newPost.save();
+      this.transitionTo('admin');
+    },
+    deletePost(post) {
+      post.destroyRecord();
       this.transitionTo('admin');
     }
   }
