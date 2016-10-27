acca388650892f01a7e0eb0a0bd1ff4f589db4d8
Ryan Loos and Stephen Newkirk <ryan@>
2016-10-27 11:27:02 -0700

    add display comments

--- app/routes/post.js
+++ app/routes/post.js
@@ -2,19 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('post', params.post_id);
+    return Ember.RSVP.hash({
+    comments: this.store.findAll('comment'),
+    post: this.store.findRecord('post', params.post_id)
+    });
   },
   actions: {
     saveComment(params) {
       var newComment = this.store.createRecord('comment', params);
       var post = params.post;
-      console.log(params.post);
-      console.log(newComment);
       post.get('comments').addObject(newComment);
       newComment.save().then(function() {
         return post.save();
       });
-      this.transitionTo('post', post);
+      this.transitionTo('post', model.post);
     }
   }
 });
